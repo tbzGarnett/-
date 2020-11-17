3b21f648b619082f9037b7393510ca7a3300d64b
陶炳志 <15858249675@163.com>
2020-11-17 17:51:43 +0800

    Initial Commit

---
+++ 华为机考题/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  华为机考题
+//
+//  Created by 陶炳志 on 2020/11/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
